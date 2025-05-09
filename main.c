4bead6f92992c8048aeaaf36c98225b89fd1031f
Pavel <shridharapavel@gmail.com>
2025-05-08 23:12:28 -0400

    Cost Function and Initial Predictions

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+# define N 5
+
+struct Data{
+  int x;
+  int y;
+};
 
 int main(void)
 {
-  printf("Hello world\n");
-  return 0;
+  struct Data arr[N] = {
+    {1, 3},
+    {3, 9},
+    {9, 27},
+    {27, 81},
+    {81, 243}
+  };
+
+  float weight = 0.00000045;
+
+  // y = x * w + b
+
+  for(int i = 0; i < N; i++)
+  {
+    float pred = arr[i].x * weight;
+
+    printf("cost: %f ", arr[i].y - pred);
+    printf("Prediction: %f ", pred);
+    printf("Actual: %d\n", arr[i].y);
+    
+  }
+  
+   return 0;
 }
