4402be3470ea01c0136135a81158b29b2f4dade6
Pavel <shridharapavel@gmail.com>
2025-05-12 00:19:08 -0400

    Used temp_params, refactored MSE to be less verbose, added more biases

--- src/xor.c
+++ src/xor.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct Data {
   int x1;
@@ -23,26 +25,29 @@ struct Params {
   float bias3;
 };
 
+float randw() { return ((rand()/(float)RAND_MAX) - 0.5f) * 0.2f; }
+
 float sigmoid(float x)
 {
   return  1/(1 + exp(-x));
 }
 
 
-float mse(float weight1, float weight2, float weight3, float weight4, float weight5, float weight6,  float bias1, float bias2,  struct Data arr[4])
+float mse(struct Params params,  struct Data arr[4])
 {
   float temp = 0;
   for(int i = 0; i < 4; i++)
   {
-    float output_1  = (weight1 * arr[i].x1) + (weight2 * arr[i].x2) + bias1;
+    float output_1  = (params.weight1 * arr[i].x1) + (params.weight2 * arr[i].x2) + params.bias1;
 
-    float output_2 = (weight3 * arr[i].x1) + (weight4 * arr[i].x2) + bias2;
+    float output_2 = (params.weight3 * arr[i].x1) + (params.weight4 * arr[i].x2) + params.bias2;
     output_1 = sigmoid(output_1);
 
     output_2 = sigmoid(output_2);
-    float pred = weight5 * output_1 + weight6 * output_2;
-    
-    float tempError = arr[i].y - pred;
+    float pred = params.weight5 * output_1 + params.weight6 * output_2 + params.bias3;
+
+    pred = sigmoid(pred);
+     float tempError = arr[i].y - pred;
 
     tempError *= tempError;
     
@@ -54,6 +59,7 @@ float mse(float weight1, float weight2, float weight3, float weight4, float weig
 
 int main(void)
 {
+  srand((unsigned)time(NULL));
   // for you ignorant fucks xor logic gate is just binary addition...
   // 0 + 0, 1 + 0 is all intuitive but 1 + 1 = 0 cus integer overflow, this xor gates truth table is non linear is not modellable by a single neuron or perceptron :)
   struct Data arr[4] = {
@@ -65,51 +71,103 @@ int main(void)
 
   struct Params params;
 
-  params.weight1 = 1e-3;
+  params.weight1 = randw();
 
-  params.weight2 = 1e-3;
+  params.weight2 = randw();
 
-  params.weight3 = 1e-3;
+  params.weight3 = randw();
 
-  params.weight4 = 1e-3;
+  params.weight4 = randw();
   
-  params.weight5 = 1e-3;
+  params.weight5 = randw();
 
-  params.weight6 = 1e-3;
+  params.weight6 = randw();
 
-  params.bias1 = 1e-3;
+  params.bias1 = randw();
 
-  params.bias2 = 1e-3;
-  float h = 1e-5;
-  float learning_rate = 1e-3;
-  for(int i = 0; i < 1000000 + 1000; i++)
+  params.bias2 = randw();
+
+  params.bias3 = randw();
+  
+  float h = 1e-3;
+  float learning_rate = 1e-1;
+  for(int i = 0; i < 100000; i++)
   {
-    float fa =  mse(params.weight1, params.weight2, params.weight3, params.weight4, params.weight5, params.weight6, params.bias1, params.bias2,  arr);
+       float fa = mse(params, arr); // Use the correct mse signature
 
-    float gradient1 = (mse(params.weight1+h, params.weight2, params.weight3, params.weight4, params.weight5, params.weight6, params.bias1, params.bias2,  arr) - fa)/h;
-    float gradient2 = (mse(params.weight1, params.weight2+h, params.weight3, params.weight4, params.weight5, params.weight6, params.bias1, params.bias2,  arr) - fa)/h;
-    float gradient3 = (mse(params.weight1, params.weight2, params.weight3+h, params.weight4, params.weight5, params.weight6, params.bias1, params.bias2,  arr) - fa)/h;
-    float gradient4 = (mse(params.weight1, params.weight2, params.weight3, params.weight4+h, params.weight5, params.weight6, params.bias1, params.bias2,  arr) - fa)/h;
-    float gradient5 = (mse(params.weight1, params.weight2, params.weight3, params.weight4, params.weight5+h, params.weight6, params.bias1, params.bias2,  arr) - fa)/h;
-    float gradient6 = (mse(params.weight1, params.weight2, params.weight3, params.weight4, params.weight5, params.weight6+h, params.bias1, params.bias2,  arr) - fa)/h;
-    float gradient7 = (mse(params.weight1, params.weight2, params.weight3, params.weight4, params.weight5, params.weight6, params.bias1+h, params.bias2,  arr) - fa)/h;
-   
-    float gradient8 = (mse(params.weight1, params.weight2, params.weight3, params.weight4, params.weight5, params.weight6, params.bias1+h, params.bias2,  arr) - fa)/h;
-   
-    params.weight1 = params.weight1 - learning_rate * gradient1;
-    
-    params.weight2 = params.weight2 - learning_rate * gradient2;
-    params.weight3 = params.weight3 - learning_rate * gradient3;
-   
-    params.weight4 = params.weight4 - learning_rate * gradient4;
-    params.weight5 = params.weight5 - learning_rate * gradient5;
-    params.weight6 = params.weight6 - learning_rate * gradient6;
-    
+        // --- Calculate Gradients using numerical 
+        struct Params temp_params;
+        float fb; // To store the MSE with the perturbed parameter
 
-    params.bias1 = params.bias1 - learning_rate * gradient7;
+        // Gradient for weight1
+        temp_params = params;
+        temp_params.weight1 += h;
+        fb = mse(temp_params, arr);
+        float gradient_w1 = (fb - fa) / h;
 
-    params.bias2 -= learning_rate * gradient8;
-    printf("cost: %f \n", mse(params.weight1, params.weight2, params.weight3, params.weight4, params.weight5,  params.weight6, params.bias1, params.bias2,  arr));
+        // Gradient for weight2
+        temp_params = params;
+        temp_params.weight2 += h;
+        fb = mse(temp_params, arr);
+        float gradient_w2 = (fb - fa) / h;
+
+        // Gradient for bias1
+        temp_params = params;
+        temp_params.bias1 += h;
+        fb = mse(temp_params, arr);
+        float gradient_b1 = (fb - fa) / h;
+
+        // Gradient for weight3
+        temp_params = params;
+        temp_params.weight3 += h;
+        fb = mse(temp_params, arr);
+        float gradient_w3 = (fb - fa) / h;
+
+        // Gradient for weight4
+        temp_params = params;
+        temp_params.weight4 += h;
+        fb = mse(temp_params, arr);
+        float gradient_w4 = (fb - fa) / h;
+
+        // Gradient for bias2
+        temp_params = params;
+        temp_params.bias2 += h;
+        fb = mse(temp_params, arr);
+        float gradient_b2 = (fb - fa) / h;
+
+        // Gradient for weight5
+        temp_params = params;
+        temp_params.weight5 += h;
+        fb = mse(temp_params, arr);
+        float gradient_w5 = (fb - fa) / h;
+
+        // Gradient for weight6
+        temp_params = params;
+        temp_params.weight6 += h;
+        fb = mse(temp_params, arr);
+        float gradient_w6 = (fb - fa) / h;
+
+        // Gradient for bias3 (the output layer bias)
+        temp_params = params;
+        temp_params.bias3 += h;
+        fb = mse(temp_params, arr);
+        float gradient_b3 = (fb - fa) / h;
+
+
+        // --- Update Parameters using Gradient Descent ---
+        params.weight1 -= learning_rate * gradient_w1;
+        params.weight2 -= learning_rate * gradient_w2;
+        params.bias1 -= learning_rate * gradient_b1;
+
+        params.weight3 -= learning_rate * gradient_w3;
+        params.weight4 -= learning_rate * gradient_w4;
+        params.bias2 -= learning_rate * gradient_b2;
+
+        params.weight5 -= learning_rate * gradient_w5;
+        params.weight6 -= learning_rate * gradient_w6;
+        params.bias3 -= learning_rate * gradient_b3; // Update the output bias
+
+        printf("cost: %f \n", mse(params,  arr));
   }
   return 0;
 }
